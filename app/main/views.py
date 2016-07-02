6d8db970e97c5bb09e0f84c76387f27fc1d8d7b6
theLastTrain <wangzhihao9110@gmail.com>
2016-07-02 23:35:19 +0800

    1. modify css of homepage by using bootstrap grid system 2. fix a typo in route'show_all'

--- app/main/views.py
+++ app/main/views.py
@@ -47,25 +47,24 @@ def index():
         error_out=False)
     posts = pagination.items
     changelogs = Changelog.query.order_by(Changelog.timestamp.desc())[0:9]
-    return render_template('index.html', pagination=pagination, posts=posts, changelogs=changelogs)
+    return render_template('index.html',
+                           pagination=pagination, posts=posts, changelogs=changelogs, show_followed=show_followed)
 
 
 @main.route('/all')
 @login_required
 def show_all():
     resp = make_response(redirect(url_for('.index')))
-    resp.ser_cookie('show_followed', '', max_age=30 * 24 * 60 * 60)
+    resp.set_cookie('show_followed', '', max_age=30 * 24 * 60 * 60)
     return resp
 
 
 @main.route('/followed')
 @login_required
 def show_followed():
-    resp = make_response(redirect(url_for('./index')))
+    resp = make_response(redirect(url_for('.index')))
     resp.set_cookie('show_followed', '1', max_age=30 * 24 * 60 * 60)
     return resp
-
-
 
 
 @main.route('/admin')
